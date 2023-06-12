f404be64ffbbbd9a27a89c2bc10e729bf35ab43b
SanjogRijal <sanjogrijal15@gmail.com>
2023-06-13 00:40:44 +0545

    feat&refactor

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,49 @@
-import {Scene, WebGLRenderer, CapsuleGeometry ,TorusGeometry, Material, Mesh, PerspectiveCamera, MeshBasicMaterial} from 'three'
+import {
+  Scene,
+  WebGLRenderer,
+  Mesh,
+  MeshBasicMaterial,
+  TorusGeometry,
+} from "three";
+import animateSRP from "./utils/animate";
+import CameraComponent from "./Components/camera";
+import SRPRenderer from "./Components/renderer";
+import SRPScene from "./Components/scene";
+import SRPCreateObjects from "./Components/Objects/SRPObjects";
+import camera from "./Components/camera";
+import scene from "./Components/scene";
 
-const scene = new Scene()
-const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const canvas = document.getElementById('bgSrPortfolio') || document.createElement('canvas');
+(function main() {
+  const canvas =
+    document.getElementById("bgSrPortfolio") ||
+    document.createElement("canvas");
+  const renderer = SRPRenderer({ rendererType: "webGL", canvas });
 
-const renderer = new WebGLRenderer({
-    canvas
-});
+  let geometry = new TorusGeometry(0.8, 0.3, 10, 200);
+  // Does not require lightning
+  const material = new MeshBasicMaterial({ color: 0xff6347, wireframe: true });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(3)
-let geometry = new CapsuleGeometry(0.5,0.5,22,20);
-const material = new MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-const capsule = new Mesh(geometry, material)
-scene.add(capsule)
+  //Requires Lightning
+  // const material = new MeshStandardMaterial({color: 0xFF6347})
+  // const pointLight = new PointLight(0xffffff)
+  // pointLight.position.set(20,20,20)
 
-//
-function animate(){
-    requestAnimationFrame(animate);
-    console.log(capsule.position)
-    const xChange = 0.01;
-    const yChange = 0.005;
-    const zChange = 0.01;
-    if(capsule.position.z > 1 ){
-    capsule.position.x += xChange;
-    capsule.position.y += yChange;
-    capsule.position.z += zChange;
-        
-} else {
-        capsule.position.x -= xChange;
-        capsule.position.y -= yChange;
-        capsule.position.z -= zChange;
+  const torusObject = SRPCreateObjects({ geometry, material });
+  let myObjects = [torusObject];
+  const scene = new Scene();
 
-    }
+  const camera = CameraComponent({
+    cameraType: "Perspective",
+    verticalFieldOfView: 75,
+    frustum: { FAR: 0.1, NEAR: 1000 },
+  });
+  SRPScene(scene, myObjects);
 
-    renderer.render(scene, camera)
-
-}
-
-animate()
+  //We don't want to have to re-render so we call animateSRP recursively in order to repaint.
+  animateSRP({
+    objectToRender: torusObject,
+    renderer,
+    scene,
+    camera,
+  });
+})();
