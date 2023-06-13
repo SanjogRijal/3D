eb65a991f5e675c46283f2561e11412e4d041a1d
SanjogRijal <sanjogrijal15@gmail.com>
2023-06-14 02:37:10 +0545

    major changes

--- src/Components/Contents/Header.ts
+++ src/Components/Contents/Header.ts
@@ -1,11 +1,11 @@
-const HeaderComponent = () => {
-  const h1 = document.createElement("h1");
-  const style = "position: absolute; left: 100px; top: 100px; color: white";
-  h1.setAttribute("style", style);
-  h1.innerHTML = "3D By Sanjog Rijal";
+// const HeaderComponent = () => {
+//   const h1 = document.createElement("h1");
+//   const style = "position: absolute; left: 100px; top: 100px; color: white";
+//   h1.setAttribute("style", style);
+//   h1.innerHTML = "3D By Sanjog Rijal";
 
-  const parentElement = document.querySelector("#app");
-  parentElement?.appendChild(h1);
-};
+//   const parentElement = document.querySelector("#app");
+//   parentElement?.appendChild(h1);
+// };
 
-export default HeaderComponent;
+// export default HeaderComponent;
